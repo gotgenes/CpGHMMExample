a1266d89d4d40e35e7f891d55506236c444ffef2
Chris Lasher <chris.lasher@gmail.com>
2012-08-09 14:45:13 -0400

    Fixes description string.

--- setup.py
+++ setup.py
@@ -11,24 +11,24 @@ SCRIPTS = [os.path.sep.join(('scripts', script)) for script in
 REQUIREMENTS = ['matplotlib', 'numpy']
 
 setup(
-    name='CpGHMMExample',
-    version='0.1a1',
-    author='Christopher D. Lasher',
-    author_email='chris.lasher@gmail.com',
-    install_requires=REQUIREMENTS,
-    packages=['CpGHMMExample'],
-    scripts=SCRIPTS,
-    url='http://pypi.python.org/pypi/CpGHMMExample',
-    license='MIT License',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
-    ],
-    description=("Python bioinformatics tools for the Ovcharenko group"),
-    long_description=open('README.rst').read(),
+        name='CpGHMMExample',
+        version='0.1a1',
+        author='Christopher D. Lasher',
+        author_email='chris.lasher@gmail.com',
+        install_requires=REQUIREMENTS,
+        packages=['CpGHMMExample'],
+        scripts=SCRIPTS,
+        url='http://pypi.python.org/pypi/CpGHMMExample',
+        license='MIT License',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+        ],
+        description=("An example of an HMM to detect CpG sites."),
+        long_description=open('README.rst').read(),
 )
 
